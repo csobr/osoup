48d73c51ab085d4b50827c5e125d4acd2206996d
csobr <siham.hadi@live.se>
2020-10-09 09:12:43 +0200

    Track view

--- pages/trackorder.tsx
+++ pages/trackorder.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Link from 'next/link';
 import styles from '../styles/Track.module.css';
 export default function TrackOrder() {
   return (
@@ -7,17 +8,32 @@ export default function TrackOrder() {
         <title>Product Detail</title>
       </Head>
       <img src='./map1.png' alt='' className={styles.productImage} />
-      <h3>Your order is being Preperd</h3>
-      <section className={styles.progress}>
-        <img src='./icons/step1.png' />
-        <div className={styles.bar} />
-        <img src='./icons/step2.png' />
-        <div className={styles.bar} />
-        <img src='./icons/step3.png' />
-        <div className={styles.bar} />
-        <img src='./icons/step4.png' />
+      <section className={styles.container}>
+        <h3 style={{ marginBottom: 0 }}>Your order is being Preperd</h3>{' '}
+        <div
+          style={{
+            margin: 0,
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            width: 150,
+          }}>
+          <p>Arrives in</p>
+          <p style={{ color: '#20d994' }}>25-30 min</p>
+        </div>
+        <section className={styles.progress}>
+          <img src='./icons/step1.png' />
+          <div className={styles.bar} />
+          <img src='./icons/step2.png' />
+          <div style={{ backgroundColor: '#C4C4C4' }} className={styles.bar} />
+          <img src='./icons/step3.png' />
+          <div style={{ backgroundColor: '#C4C4C4' }} className={styles.bar} />
+          <img src='./icons/step4.png' />
+        </section>
       </section>
-      <button className={styles.button}>View your receipt</button>
+      <Link href='./receipt'>
+        <button className={styles.button}>View your receipt</button>
+      </Link>
     </div>
   );
 }
